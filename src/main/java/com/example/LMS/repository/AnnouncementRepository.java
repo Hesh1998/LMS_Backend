6cad16acefbc991222526dc31cafe3bdd8af9f3d
Heshan Nanayakkara <kalaruheshan@gmail.com>
2022-10-14 18:03:08 +0530

    AnnouncementRepository added by Heshan

---
+++ src/main/java/com/example/LMS/repository/AnnouncementRepository.java
@@ -0,0 +1,15 @@
+package com.example.LMS.repository;
+
+import com.example.LMS.model.Announcement;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AnnouncementRepository extends JpaRepository<Announcement, Integer> {
+    @Query(value = "SELECT a FROM Announcement a WHERE a.course.course_id = ?1")
+    List<Announcement> findAnnouncementsByCourseId(Integer id);
+}
+
